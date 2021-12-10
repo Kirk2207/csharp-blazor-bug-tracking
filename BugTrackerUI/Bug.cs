365cd2142eadc93e48dd8257ae0e208d03b95f3d
Kirk <kirk.gallyer@dhl.com>
2021-12-10 16:28:01 +0000

    Module 4 Complete

--- BugTrackerUI/Bug.cs
+++ BugTrackerUI/Bug.cs
@@ -1,19 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace BugTrackerUI
-{
-    public class Bug
-    {
+﻿namespace BugTrackerUI {
+    public class Bug {
         public int Id { get; set; }
-
         public string Title { get; set; }
-
         public string Description { get; set; }
-
         public int Priority { get; set; }
     }
 }
